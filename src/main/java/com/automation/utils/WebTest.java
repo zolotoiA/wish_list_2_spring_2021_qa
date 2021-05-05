cd6ef6ad2c3898efcb6a7dcfef93305621832400
Andrei Zolotoi <andrei.zolotoi@endava.com>
2021-05-05 15:02:53 +0300

    1. Add webdrivermanager,JUnit,selenium dependency 2. Add WebTest class to test

---
+++ src/main/java/com/automation/utils/WebTest.java
@@ -0,0 +1,34 @@
+package com.automation.utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class WebTest {
+
+    private WebDriver driver;
+
+    @BeforeClass
+    public static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @Before
+    public void setupTest() {
+        driver = new ChromeDriver();
+    }
+
+    @After
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void test() {
+        // Your test code here
+    }
+}
