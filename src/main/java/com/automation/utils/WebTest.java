3824778567b23ec13c99f6d395aedf2bdb94f2a3
Andrei Zolotoi <andrei.zolotoi@endava.com>
2021-05-05 16:52:06 +0300

    1. Add webdrivermanager,JUnit,selenium dependency 2. Add WebTest class to test

--- src/main/java/com/automation/utils/WebTest.java
+++ src/main/java/com/automation/utils/WebTest.java
@@ -5,6 +5,9 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 
 public class WebTest {
 
